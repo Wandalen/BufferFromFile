27ffa8fd8040eafaf9e508579124abf6681c3a6f
Unknown <volodymyr.migdal@gmail.com>
2017-05-26 10:29:24 +0300

    few renames in sample

--- sample/FlushExperiment.js
+++ sample/FlushExperiment.js
@@ -12,17 +12,19 @@ catch( err )
 // mmap file
 
 var filePath = __dirname + '/TestFile.txt';
-var buffer = BufferFromFile( filePath );
-var arrayBuffer = buffer.Uint8Array();
+var descriptor = BufferFromFile( filePath );
+var buffer = descriptor.Uint8Array();
 
-arrayBuffer[ 0 ] = 48 + Math.round( Math.random()*9 );
+buffer[ 0 ] = 48 + Math.round( Math.random()*9 );
 
-BufferFromFile.flush( arrayBuffer );
+BufferFromFile.flush( buffer );
+
+console.log( "OK" );
+
+buffer[ 0 ] = 48 + Math.round( Math.random()*9 );
+
+BufferFromFile.flush({ buffer : buffer } );
 
 console.log( "OK" );
 
-arrayBuffer[ 0 ] = 48 + Math.round( Math.random()*9 );
-
-BufferFromFile.flush({ buffer : arrayBuffer } );
-
-console.log( "OK" );
+BufferFromFile.unmap( buffer );
