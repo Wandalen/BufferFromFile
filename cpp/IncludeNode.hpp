d02c0b6de1566705ea6a0cb5b1cbbcb3e532b86a
volodymyrmigdal <volodymyr.migdal@gmail.com>
2020-05-25 19:29:51 +0300

    move common c++ files to NjsCppBasic

--- cpp/IncludeNode.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _IncludeNode_hpp_
-#define _IncludeNode_hpp_
-
-#include <v8.h>
-#include <uv.h>
-#include <node.h>
-// #include <nan.h>
-
-#endif // _IncludeNode_hpp_
--- cpp/wTools/zAll.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef _wTools_zAll_hpp_
-#define _wTools_zAll_hpp_
-
-#include "./aBase.hpp"
-#include "./V8.hpp"
-
-#endif // _wTools_All_hpp_
