08cbbacecec29752f258c7753b5e8c709c5e3626
volodymyrmigdal <volodymyr.migdal@gmail.com>
2020-05-27 17:48:47 +0300

    update sample

--- sample/Sample.js
+++ sample/Sample.js
@@ -1,9 +1,23 @@
+var BufferFromFile = require( 'bufferfromfile' );
 
-if( typeof module !== 'undefined' )
-{
-  require( 'wTools' );
-}
+// mmap file
 
-var _ = wTools;
+var filePath = __dirname + '/TestFile.txt';
+var buffer = BufferFromFile( filePath ).NodeBuffer();
 
-/**/
+console.log( 'buffer.length :', buffer.length );
+console.log( 'buffer.toString :', buffer.toString() );
+
+// update buffer data
+
+buffer[ 0 ] = 48;
+
+// flush written data to make sure data on will get on hard drive shortly
+
+BufferFromFile.flush( buffer );
+
+// unmap file
+
+BufferFromFile.unmap( buffer );
+
+console.log( '\nTry : cat sample/TestFile.txt' );
